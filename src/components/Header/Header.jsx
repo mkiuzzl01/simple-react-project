cbea4f650680d7ac4bae231e523aa1e297ae071f
mkiuzzal01 <mkiuzzal07@gmail.com>
2024-10-06 14:21:31 +0600

    navar toggle implement for sm device

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,26 @@ import image from "../../assets/banner.jpg";
 const Header = () => {
   return (
     <div
-      className="h-[700px] bg-cover bg-center pt-40 px-10 space-y-5 relative"
+      className="h-[40rem] bg-cover bg-center pt-24 md:px-10 space-y-5 relative"
       style={{ backgroundImage: `url(${image})` }}
     >
-      <h1 className="text-6xl">Discover the Latest <br /> in Tech Innovations</h1>
-      <p>
-        Shop cutting-edge gadgets, electronics, and accessories at unbeatable
-        prices. <br /> Elevate your tech game with our exclusive collections—where
-        innovation meets affordability
-      </p>
-      <div>
-        <button className="px-4 py-2 bg-[#dc7516] rounded-2xl hover:text-white hover:bg-[#b46c29] ">Resister Now</button>
+      <div className="absolute inset-0 bg-white opacity-50 z-0"></div>
+      <div className="relative z-10 space-y-7 p-10">
+        <div className="space-y-7">
+          <h1 className="text-5xl font-semibold">
+            Discover the Latest <br /> in Tech Innovations
+          </h1>
+          <p>
+            Shop cutting-edge gadgets, electronics, and accessories at
+            unbeatable prices. <br /> Elevate your tech game with our exclusive
+            collections—where innovation meets affordability
+          </p>
+        </div>
+        <div>
+          <button className="px-4 py-2 bg-[#ffad60] rounded-full hover:text-white hover:bg-[#dc7516] ">
+            Resister Now
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import menu_icon from "../.././assets/menu-alt-2-svgrepo-com.svg";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = (toggle) => {
+    setToggle(toggle);
+  };
+
+  useEffect(() => {
+
+    //Menu Reset function
+    const handleResetSmallMenu = () => {
+      if (window.innerWidth >= 1024) {
+        // console.log("ok");
+        setToggle(false);
+      }
+    };
+    return () => window.addEventListener("resize", handleResetSmallMenu);
+  }, []);
+
   return (
-    <div className="fixed top-0 left-0 right-0 z-10 max-w-7xl m-auto ">
-      <div className="flex items-center justify-between p-5 bg-green-300 bg-opacity-50">
-        <div>
-          <h1 className="text-3xl font-bold">E-Buy</h1>
+    <div className="fixed top-0 left-0 right-0 z-20 max-w-7xl m-auto ">
+      <div className="flex items-center justify-between p-4 bg-green-300 bg-opacity-50">
+        <div className="inline-flex items-center space-x-1 ">
+          <div onClick={() => handleToggle(!toggle)}>
+            <img src={menu_icon} alt="icon" className="w-7 lg:hidden" />
+          </div>
+          <h1 className="lg:text-3xl text-xl font-bold">E-Buy</h1>
         </div>
-        <div>
+
+        {/* for large device */}
+        <div className="hidden lg:block">
           <ul className="flex space-x-4 cursor-pointer">
             <li>
               <a href="/Home"></a>Home
@@ -24,8 +48,31 @@ const Navbar = () => {
           </ul>
         </div>
         <div>
-          <button className="px-4 py-2 rounded-2xl bg-[#e3c33f] hover:text-white hover:bg-[#d5b535]">Login</button>
+          <button className="px-4 py-2 rounded-full bg-[#ffeead] hover:text-white hover:bg-[#ecd05d]">
+            Login
+          </button>
         </div>
+      </div>
+      {/* for sm device */}
+      <div
+        className={`${
+          toggle ? "duration-300" : "-translate-x-36 duration-300"
+        } bg-[#fff7d1]  w-32 rounded-md ps-3 py-2 ms-2 mt-2 lg:hidden`}
+      >
+        <ul className="cursor-pointer">
+          <li>
+            <a href="/Home"></a>Home
+          </li>
+          <li>
+            <a href="/About"></a>About
+          </li>
+          <li>
+            <a href="/Products"></a>Products
+          </li>
+          <li>
+            <a href="/Contact"></a>Contact
+          </li>
+        </ul>
       </div>
     </div>
   );
